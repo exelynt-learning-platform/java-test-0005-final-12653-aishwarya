ac8418d3214692306e201861303e11edaf8edca9
Aishwarya Naik <naikaishwarya930@gmail.com>
2026-03-14 16:18:18 +0530

    Add files via upload

---
+++ JAVATEST0005_AishwaryaNaik.java
@@ -0,0 +1,22 @@
+package ExelyntAssignment_AishwaryaNaik;
+
+public class JAVATEST0005_AishwaryaNaik {
+	
+	public static void main(String[] args) {
+
+        int num = 1;
+        int rows = 5;
+
+        for (int i = 1; i <= rows; i++) {
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(num + " ");
+                num++;
+            }
+
+            System.out.println();
+        }
+    }
+
+
+}
